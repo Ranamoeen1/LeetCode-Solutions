8d41ca30be17cfd84b89a1e3b0c0758c431a9f34
Rana Moeen <ranamoeenodeen@gmail.com>
2026-03-05 18:51:22 +0500

    Time: 7 ms (83.47%), Space: 19.2 MB (93.82%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
@@ -1,42 +1,42 @@
-class Solution:
-    def minOperations(self, s: str) -> int:
-        # Count operations for pattern starting with '0'
-        operations_start_with_0 = 0
-        
-        # Count operations for pattern starting with '1'
-        operations_start_with_1 = 0
-        
-        for i in range(len(s)):
-            
-            if i % 2 == 0:
-                if s[i] != '0':
-                    operations_start_with_0 += 1
-                else:
-                    operations_start_with_1 += 1
-            else:  
-                if s[i] != '1':
-                    operations_start_with_0 += 1
-                else:
-                    operations_start_with_1 += 1
-        
-        return min(operations_start_with_0, operations_start_with_1)
-
 # class Solution:
 #     def minOperations(self, s: str) -> int:
-#         n = len(s)
+#         # Count operations for pattern starting with '0'
 #         operations_start_with_0 = 0
         
-#         for i in range(n):
-#             # Expected character for pattern starting with '0'
-#             expected = '0' if i % 2 == 0 else '1'
-            
-#             if s[i] != expected:
-#                 operations_start_with_0 += 1
+#         # Count operations for pattern starting with '1'
+#         operations_start_with_1 = 0
         
-#         # The other pattern will need the remaining operations
-#         operations_start_with_1 = n - operations_start_with_0
+#         for i in range(len(s)):
+            
+#             if i % 2 == 0:
+#                 if s[i] != '0':
+#                     operations_start_with_0 += 1
+#                 else:
+#                     operations_start_with_1 += 1
+#             else:  
+#                 if s[i] != '1':
+#                     operations_start_with_0 += 1
+#                 else:
+#                     operations_start_with_1 += 1
         
 #         return min(operations_start_with_0, operations_start_with_1)
+
+class Solution:
+    def minOperations(self, s: str) -> int:
+        n = len(s)
+        operations_start_with_0 = 0
+        
+        for i in range(n):
+            # Expected character for pattern starting with '0'
+            expected = '0' if i % 2 == 0 else '1'
+            
+            if s[i] != expected:
+                operations_start_with_0 += 1
+        
+        # The other pattern will need the remaining operations
+        operations_start_with_1 = n - operations_start_with_0
+        
+        return min(operations_start_with_0, operations_start_with_1)
 
 
 # class Solution:
