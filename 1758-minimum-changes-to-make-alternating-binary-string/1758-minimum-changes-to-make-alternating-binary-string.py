e2ba9426eea6e388561ac0a1fe3dae39f5690c18
Rana Moeen <ranamoeenodeen@gmail.com>
2026-03-05 18:48:41 +0500

    Time: 9 ms (47.81%), Space: 19.2 MB (83.47%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
@@ -1,25 +1,25 @@
-# class Solution:
-#     def minOperations(self, s: str) -> int:
-#         # Count operations for pattern starting with '0'
-#         operations_start_with_0 = 0
+class Solution:
+    def minOperations(self, s: str) -> int:
+        # Count operations for pattern starting with '0'
+        operations_start_with_0 = 0
         
-#         # Count operations for pattern starting with '1'
-#         operations_start_with_1 = 0
+        # Count operations for pattern starting with '1'
+        operations_start_with_1 = 0
         
-#         for i in range(len(s)):
+        for i in range(len(s)):
             
-#             if i % 2 == 0:
-#                 if s[i] != '0':
-#                     operations_start_with_0 += 1
-#                 else:
-#                     operations_start_with_1 += 1
-#             else:  
-#                 if s[i] != '1':
-#                     operations_start_with_0 += 1
-#                 else:
-#                     operations_start_with_1 += 1
+            if i % 2 == 0:
+                if s[i] != '0':
+                    operations_start_with_0 += 1
+                else:
+                    operations_start_with_1 += 1
+            else:  
+                if s[i] != '1':
+                    operations_start_with_0 += 1
+                else:
+                    operations_start_with_1 += 1
         
-#         return min(operations_start_with_0, operations_start_with_1)
+        return min(operations_start_with_0, operations_start_with_1)
 
 # class Solution:
 #     def minOperations(self, s: str) -> int:
@@ -79,12 +79,14 @@
 
 
 
-class Solution:
-    def minOperations(self, s: str) -> int:
-        n = len(s)
+# class Solution:
+#     def minOperations(self, s: str) -> int:
+#         n = len(s)
         
-        # Count mismatches for pattern starting with '0'
-        count = sum(1 for i, ch in enumerate(s) 
-                    if (i % 2 == 0 and ch != '0') or (i % 2 == 1 and ch != '1'))
+#         # Count mismatches for pattern starting with '0'
+#         count = sum(1 for i, ch in enumerate(s) 
+#                     if (i % 2 == 0 and ch != '0') or (i % 2 == 1 and ch != '1'))
         
-        return min(count, n - count)
+#         return min(count, n - count)
+
+
