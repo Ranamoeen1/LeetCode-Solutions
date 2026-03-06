1e4c126e609e1495abcb23d2fb54296a336cde31
Rana Moeen <ranamoeenodeen@gmail.com>
2026-03-06 14:46:42 +0500

    Time: 0 ms (100%), Space: 19.2 MB (77.09%) - LeetHub

--- 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.py
+++ 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.py
@@ -1,8 +1,21 @@
+# class Solution:
+#     def checkOnesSegment(self, s: str) -> bool:
+#         zero_index = s.find('0')
+        
+#         if zero_index == -1:
+#             return True
+        
+#         return '1' not in s[zero_index:]
+
 class Solution:
     def checkOnesSegment(self, s: str) -> bool:
-        zero_index = s.find('0')
+     
+        found_zero = False
         
-        if zero_index == -1:
-            return True
+        for i in range(len(s)):
+            if s[i] == '0':
+                found_zero = True
+            elif found_zero and s[i] == '1':
+                return False
         
-        return '1' not in s[zero_index:]
+        return True
