b7ea195913ad31a5895bab217d89cd8efb326a11
Rana Moeen <ranamoeenodeen@gmail.com>
2026-03-08 22:06:53 +0500

    Time: 4 ms (19.88%), Space: 19.3 MB (90.94%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.py
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.py
@@ -1,45 +1,45 @@
-# class Solution:
-#     def findDifferentBinaryString(self, nums: List[str]) -> str:
-#         n = len(nums)
-#         result = []
-        
-#         # Construct a string that differs from each string at position i
-#         for i in range(n):
-#             # Take the opposite of the i-th character in the i-th string
-#             if nums[i][i] == '0':
-#                 result.append('1')
-#             else:
-#                 result.append('0')
-        
-#         return ''.join(result)
-
-
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
         n = len(nums)
-        num_set = set(nums)
+        result = []
         
-        def backtrack(current):
-            if len(current) == n:
-                candidate = ''.join(current)
-                if candidate not in num_set:
-                    return candidate
-                return None
+        # Construct a string that differs from each string at position i
+        for i in range(n):
+            # Take the opposite of the i-th character in the i-th string
+            if nums[i][i] == '0':
+                result.append('1')
+            else:
+                result.append('0')
+        
+        return ''.join(result)
+
+
+# class Solution:
+#     def findDifferentBinaryString(self, nums: List[str]) -> str:
+#         n = len(nums)
+#         num_set = set(nums)
+        
+#         def backtrack(current):
+#             if len(current) == n:
+#                 candidate = ''.join(current)
+#                 if candidate not in num_set:
+#                     return candidate
+#                 return None
             
-            # Try '0' first
-            current.append('0')
-            result = backtrack(current)
-            if result:
-                return result
-            current.pop()
+#             # Try '0' first
+#             current.append('0')
+#             result = backtrack(current)
+#             if result:
+#                 return result
+#             current.pop()
             
-            # Try '1'
-            current.append('1')
-            result = backtrack(current)
-            if result:
-                return result
-            current.pop()
+#             # Try '1'
+#             current.append('1')
+#             result = backtrack(current)
+#             if result:
+#                 return result
+#             current.pop()
             
-            return None
+#             return None
         
         return backtrack([])
