4ce7ea25a9f91ffde3833783afbaf82f7fd4c049
Rana Moeen <ranamoeenodeen@gmail.com>
2026-03-18 14:15:42 +0500

    Time: 247 ms (88.28%), Space: 66.3 MB (62.07%) - LeetHub

--- 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.py
+++ 3070-count-submatrices-with-top-left-element-and-sum-less-than-k/3070-count-submatrices-with-top-left-element-and-sum-less-than-k.py
@@ -1,28 +1,56 @@
 class Solution:
     def countSubmatrices(self, grid: List[List[int]], k: int) -> int:
         m, n = len(grid), len(grid[0])
-        
-        # Create prefix sum matrix
-        prefix = [[0] * n for _ in range(m)]
-        
-        # Fill prefix sums
-        for i in range(m):
-            for j in range(n):
-                prefix[i][j] = grid[i][j]
-                if i > 0:
-                    prefix[i][j] += prefix[i-1][j]
-                if j > 0:
-                    prefix[i][j] += prefix[i][j-1]
-                if i > 0 and j > 0:
-                    prefix[i][j] -= prefix[i-1][j-1]
-        
-        # Count valid submatrices
         count = 0
+        
+        # Create row-wise prefix sums
+        row_prefix = [[0] * (n + 1) for _ in range(m)]
         for i in range(m):
             for j in range(n):
-                if prefix[i][j] <= k:
+                row_prefix[i][j + 1] = row_prefix[i][j] + grid[i][j]
+        
+        # For each possible ending column
+        for end_col in range(n):
+            # For each possible ending row, calculate sum of submatrix
+            total = 0
+            for end_row in range(m):
+                # Add the sum of current row from col 0 to end_col
+                total += row_prefix[end_row][end_col + 1]
+                if total <= k:
                     count += 1
                 else:
+                    # Since all values are non-negative, we can break for remaining rows
+                    # because adding more rows will only increase the sum
                     break
         
         return count
+
+
+# class Solution:
+#     def countSubmatrices(self, grid: List[List[int]], k: int) -> int:
+#         m, n = len(grid), len(grid[0])
+        
+#         # Create prefix sum matrix
+#         prefix = [[0] * n for _ in range(m)]
+        
+#         # Fill prefix sums
+#         for i in range(m):
+#             for j in range(n):
+#                 prefix[i][j] = grid[i][j]
+#                 if i > 0:
+#                     prefix[i][j] += prefix[i-1][j]
+#                 if j > 0:
+#                     prefix[i][j] += prefix[i][j-1]
+#                 if i > 0 and j > 0:
+#                     prefix[i][j] -= prefix[i-1][j-1]
+        
+#         # Count valid submatrices
+#         count = 0
+#         for i in range(m):
+#             for j in range(n):
+#                 if prefix[i][j] <= k:
+#                     count += 1
+#                 else:
+#                     break
+        
+#         return count
