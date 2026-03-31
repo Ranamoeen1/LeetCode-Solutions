3ec3b79d149ff2dd043dae33d2afb67f2d12dbd8
Rana Moeen <ranamoeenodeen@gmail.com>
2026-03-31 22:30:26 +0500

    Time: 3 ms (21.43%), Space: 19.2 MB (66.93%) - LeetHub

--- 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.py
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.py
@@ -1,11 +1,16 @@
 class Solution:
     def canBeEqual(self, s1: str, s2: str) -> bool:
-        # Check even indices (0, 2)
-        even1 = sorted([s1[0], s1[2]])
-        even2 = sorted([s2[0], s2[2]])
+        return (sorted([s1[0], s1[2]]) == sorted([s2[0], s2[2]]) and 
+                sorted([s1[1], s1[3]]) == sorted([s2[1], s2[3]]))
+
+# class Solution:
+#     def canBeEqual(self, s1: str, s2: str) -> bool:
+#         # Check even indices (0, 2)
+#         even1 = sorted([s1[0], s1[2]])
+#         even2 = sorted([s2[0], s2[2]])
         
-        # Check odd indices (1, 3)
-        odd1 = sorted([s1[1], s1[3]])
-        odd2 = sorted([s2[1], s2[3]])
+#         # Check odd indices (1, 3)
+#         odd1 = sorted([s1[1], s1[3]])
+#         odd2 = sorted([s2[1], s2[3]])
         
-        return even1 == even2 and odd1 == odd2
+#         return even1 == even2 and odd1 == odd2
