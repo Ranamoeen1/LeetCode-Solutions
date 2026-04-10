15d9bfb591f8d1dfc177072b9a33472caeda308c
Rana Moeen <ranamoeenodeen@gmail.com>
2026-04-10 21:01:15 +0500

    Time: 0 ms (100%), Space: 19.3 MB (77.52%) - LeetHub

--- 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.py
+++ 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.py
@@ -1,19 +1,19 @@
 class Solution:
     def minimumDistance(self, nums: List[int]) -> int:
-        indices = defaultdict(list)
-        for i, num in enumerate(nums):
-            indices[num].append(i)
-        
+        # Store last 3 indices seen for each value
+        last_three = defaultdict(list)  # num -> list of last 3 indices
         min_dist = float('inf')
         
-        for idx_list in indices.values():
-            if len(idx_list) >= 3:
-                for i in range(len(idx_list) - 2):
-                    # Early termination: minimum possible is 4 (indices like 0,1,2)
-                    span = idx_list[i + 2] - idx_list[i]
-                    min_dist = min(min_dist, 2 * span)
-                    if min_dist == 4:  # Can't get better than this
-                        return 4
+        for i, num in enumerate(nums):
+            last_three[num].append(i)
+            # Keep only last 3
+            if len(last_three[num]) > 3:
+                last_three[num].pop(0)
+            
+            # Check if we have 3 occurrences
+            if len(last_three[num]) == 3:
+                span = last_three[num][2] - last_three[num][0]
+                min_dist = min(min_dist, 2 * span)
         
         return -1 if min_dist == float('inf') else min_dist
 
@@ -30,32 +30,54 @@ class Solution:
         
 #         for idx_list in indices.values():
 #             if len(idx_list) >= 3:
-#                 # Sliding window of size 3 on the index list
 #                 for i in range(len(idx_list) - 2):
-#                     window_span = idx_list[i + 2] - idx_list[i]
-#                     min_dist = min(min_dist, 2 * window_span)
+#                     # Early termination: minimum possible is 4 (indices like 0,1,2)
+#                     span = idx_list[i + 2] - idx_list[i]
+#                     min_dist = min(min_dist, 2 * span)
+#                     if min_dist == 4:  # Can't get better than this
+#                         return 4
         
 #         return -1 if min_dist == float('inf') else min_dist
 
 
 
+
 # # class Solution:
 # #     def minimumDistance(self, nums: List[int]) -> int:
-# #         # Group indices by value
 # #         indices = defaultdict(list)
 # #         for i, num in enumerate(nums):
 # #             indices[num].append(i)
         
 # #         min_dist = float('inf')
         
-# #         # For each value with 3+ occurrences
 # #         for idx_list in indices.values():
 # #             if len(idx_list) >= 3:
-# #                 # Check all consecutive triplets
-# #                 # Distance for (i,j,k) where i<j<k is 2*(k-i)
+# #                 # Sliding window of size 3 on the index list
 # #                 for i in range(len(idx_list) - 2):
-# #                     # Use indices[i], indices[i+1], indices[i+2]
-# #                     dist = 2 * (idx_list[i + 2] - idx_list[i])
-# #                     min_dist = min(min_dist, dist)
+# #                     window_span = idx_list[i + 2] - idx_list[i]
+# #                     min_dist = min(min_dist, 2 * window_span)
         
-# #         return min_dist if min_dist != float('inf') else -1
+# #         return -1 if min_dist == float('inf') else min_dist
+
+
+
+# # # class Solution:
+# # #     def minimumDistance(self, nums: List[int]) -> int:
+# # #         # Group indices by value
+# # #         indices = defaultdict(list)
+# # #         for i, num in enumerate(nums):
+# # #             indices[num].append(i)
+        
+# # #         min_dist = float('inf')
+        
+# # #         # For each value with 3+ occurrences
+# # #         for idx_list in indices.values():
+# # #             if len(idx_list) >= 3:
+# # #                 # Check all consecutive triplets
+# # #                 # Distance for (i,j,k) where i<j<k is 2*(k-i)
+# # #                 for i in range(len(idx_list) - 2):
+# # #                     # Use indices[i], indices[i+1], indices[i+2]
+# # #                     dist = 2 * (idx_list[i + 2] - idx_list[i])
+# # #                     min_dist = min(min_dist, dist)
+        
+# # #         return min_dist if min_dist != float('inf') else -1
