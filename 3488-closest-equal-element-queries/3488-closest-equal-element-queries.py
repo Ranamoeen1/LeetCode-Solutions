652a64c23a1b34252ca3be472326c2e6eb41d864
Rana Moeen <ranamoeenodeen@gmail.com>
2026-04-16 19:17:44 +0500

    Time: 307 ms (72.66%), Space: 54.7 MB (78.91%) - LeetHub

--- 3488-closest-equal-element-queries/3488-closest-equal-element-queries.py
+++ 3488-closest-equal-element-queries/3488-closest-equal-element-queries.py
@@ -1,43 +1,43 @@
-# from typing import List
-# from collections import defaultdict
-# import bisect
+# # # from typing import List
+# # # from collections import defaultdict
+# # # import bisect
 
-# class Solution:
-#     def solveQueries(self, nums: List[int], queries: List[int]) -> List[int]:
-#         n = len(nums)
+# # # class Solution:
+# # #     def solveQueries(self, nums: List[int], queries: List[int]) -> List[int]:
+# # #         n = len(nums)
         
-#         # Group indices by value
-#         value_indices = defaultdict(list)
-#         for i, num in enumerate(nums):
-#             value_indices[num].append(i)
+# # #         # Group indices by value
+# # #         value_indices = defaultdict(list)
+# # #         for i, num in enumerate(nums):
+# # #             value_indices[num].append(i)
         
-#         results = []
+# # #         results = []
         
-#         for query in queries:
-#             val = nums[query]
-#             indices = value_indices[val]
+# # #         for query in queries:
+# # #             val = nums[query]
+# # #             indices = value_indices[val]
             
-#             # If this value appears only once, answer is -1
-#             if len(indices) == 1:
-#                 results.append(-1)
-#                 continue
+# # #             # If this value appears only once, answer is -1
+# # #             if len(indices) == 1:
+# # #                 results.append(-1)
+# # #                 continue
             
-#             # Find position of query index in the sorted list
-#             pos = bisect.bisect_left(indices, query)
+# # #             # Find position of query index in the sorted list
+# # #             pos = bisect.bisect_left(indices, query)
             
       
-#             prev_idx = indices[(pos - 1) % len(indices)]
-#             next_idx = indices[(pos + 1) % len(indices)]
+# # #             prev_idx = indices[(pos - 1) % len(indices)]
+# # #             next_idx = indices[(pos + 1) % len(indices)]
             
-#             # Calculate circular distances
-#             dist_prev = min(abs(query - prev_idx), n - abs(query - prev_idx))
-#             dist_next = min(abs(query - next_idx), n - abs(query - next_idx))
+# # #             # Calculate circular distances
+# # #             dist_prev = min(abs(query - prev_idx), n - abs(query - prev_idx))
+# # #             dist_next = min(abs(query - next_idx), n - abs(query - next_idx))
             
-#             # Take the minimum distance
-#             min_dist = min(dist_prev, dist_next)
-#             results.append(min_dist)
+# # #             # Take the minimum distance
+# # #             min_dist = min(dist_prev, dist_next)
+# # #             results.append(min_dist)
         
-#         return results
+# # #         return results
 
 
 from typing import List
@@ -82,3 +82,85 @@ class Solution:
         
         # Return answers only for queried indices
         return [answer[q] for q in queries]
+
+# from typing import List
+# from collections import defaultdict
+
+# class Solution:
+#     def solveQueries(self, nums: List[int], queries: List[int]) -> List[int]:
+#         n = len(nums)
+        
+#         # Track next occurrence to the right
+#         next_occurrence = [-1] * n
+#         last_seen = {}
+        
+#         # First pass: find next occurrence to the right
+#         for i in range(n):
+#             val = nums[i]
+#             if val in last_seen:
+#                 prev_idx = last_seen[val]
+#                 next_occurrence[prev_idx] = i
+#             last_seen[val] = i
+        
+#         # Second pass: handle circular wrap by checking from start
+#         # For indices that didn't find a next occurrence to the right,
+#         # look from the beginning
+#         for val, last_idx in last_seen.items():
+#             # Find first occurrence of this value
+#             first_idx = -1
+#             for i in range(n):
+#                 if nums[i] == val:
+#                     first_idx = i
+#                     break
+            
+#             if first_idx != -1 and next_occurrence[last_idx] == -1:
+#                 # Last occurrence connects to first occurrence circularly
+#                 next_occurrence[last_idx] = first_idx
+        
+#         # Track previous occurrence to the left (for circular distance)
+#         prev_occurrence = [-1] * n
+#         last_seen = {}
+        
+#         for i in range(n - 1, -1, -1):
+#             val = nums[i]
+#             if val in last_seen:
+#                 next_idx = last_seen[val]
+#                 prev_occurrence[next_idx] = i
+#             last_seen[val] = i
+        
+#         # Handle circular wrap for previous occurrences
+#         for val, first_idx in last_seen.items():
+#             # Find last occurrence of this value
+#             last_idx = -1
+#             for i in range(n - 1, -1, -1):
+#                 if nums[i] == val:
+#                     last_idx = i
+#                     break
+            
+#             if last_idx != -1 and prev_occurrence[first_idx] == -1:
+#                 # First occurrence connects to last occurrence circularly
+#                 prev_occurrence[first_idx] = last_idx
+        
+#         # Calculate answers
+#         result = [-1] * n
+        
+#         for i in range(n):
+#             min_dist = float('inf')
+            
+#             if next_occurrence[i] != -1:
+#                 # Distance to next occurrence (going right)
+#                 dist_right = next_occurrence[i] - i
+#                 # Also consider wrapping around from i to next_occurrence[i]
+#                 dist_wrap = n - dist_right
+#                 min_dist = min(min_dist, dist_right, dist_wrap)
+            
+#             if prev_occurrence[i] != -1:
+#                 # Distance to previous occurrence (going left)
+#                 dist_left = i - prev_occurrence[i]
+#                 dist_wrap = n - dist_left
+#                 min_dist = min(min_dist, dist_left, dist_wrap)
+            
+#             if min_dist != float('inf'):
+#                 result[i] = min_dist
+        
+#         return [result[q] for q in queries]
