2285bf98df8e2d7f7d25e9bbe23de458f708eac9
Rana Moeen <ranamoeenodeen@gmail.com>
2026-04-20 21:47:29 +0500

    Time: 0 ms (100%), Space: 19.3 MB (61.41%) - LeetHub

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.py
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.py
@@ -1,16 +1,37 @@
+# class Solution:
+#     def maxDistance(self, colors: List[int]) -> int:
+#         n = len(colors)
+#         max_dist = 0
+        
+#         # Check distances from first house to all other houses
+#         for i in range(n):
+#             if colors[i] != colors[0]:
+#                 max_dist = max(max_dist, i - 0)
+        
+#         # Check distances from last house to all other houses
+#         for i in range(n):
+#             if colors[i] != colors[n-1]:
+#                 max_dist = max(max_dist, (n-1) - i)
+        
+#         return max_dist
+
+
 class Solution:
     def maxDistance(self, colors: List[int]) -> int:
         n = len(colors)
-        max_dist = 0
         
-        # Check distances from first house to all other houses
-        for i in range(n):
+        # Find the farthest index from the start that has a different color
+        left_dist = 0
+        for i in range(n - 1, -1, -1):
             if colors[i] != colors[0]:
-                max_dist = max(max_dist, i - 0)
+                left_dist = i
+                break
         
-        # Check distances from last house to all other houses
+        # Find the farthest index from the end that has a different color
+        right_dist = 0
         for i in range(n):
-            if colors[i] != colors[n-1]:
-                max_dist = max(max_dist, (n-1) - i)
+            if colors[i] != colors[n - 1]:
+                right_dist = n - 1 - i
+                break
         
-        return max_dist
+        return max(left_dist, right_dist)
