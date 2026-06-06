bf56322cd898f0c929acaf824387137ad3727bda
Rana Moeen <ranamoeenodeen@gmail.com>
2026-06-06 18:38:50 +0500

    Time: 3 ms (74.11%), Space: 19.3 MB (97.66%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.py
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.py
@@ -1,17 +1,31 @@
+from itertools import accumulate
+
 class Solution:
     def leftRightDifference(self, nums: List[int]) -> List[int]:
-        left_sum = 0
-        right_sum = sum(nums)
-        answer = []
+        total = sum(nums)
         
-        for num in nums:
-            # Current number is neither to the left nor to the right of itself
-            right_sum -= num
+        # accumulate(nums) gives a running total: [nums[0], nums[0]+nums[1], ...]
+        return [abs(2 * prefix - num - total) for num, prefix in zip(nums, accumulate(nums))]
+
+
+
+
+
+
+# class Solution:
+#     def leftRightDifference(self, nums: List[int]) -> List[int]:
+#         left_sum = 0
+#         right_sum = sum(nums)
+#         answer = []
+        
+#         for num in nums:
+#             # Current number is neither to the left nor to the right of itself
+#             right_sum -= num
             
-            # Calculate the absolute difference
-            answer.append(abs(left_sum - right_sum))
+#             # Calculate the absolute difference
+#             answer.append(abs(left_sum - right_sum))
             
-            # Current number now becomes part of the left sum for the next elements
-            left_sum += num
+#             # Current number now becomes part of the left sum for the next elements
+#             left_sum += num
             
-        return answer
+#         return answer
