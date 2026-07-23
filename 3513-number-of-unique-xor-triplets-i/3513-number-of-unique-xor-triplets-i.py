fb8b8786cda97485c4f03895baf77c5e0c7bda6f
Rana Moeen <ranamoeenodeen@gmail.com>
2026-07-23 18:55:46 +0500

    Time: 0 ms (100%), Space: 31.9 MB (91.53%) - LeetHub

--- 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.py
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.py
@@ -1,6 +1,22 @@
+# class Solution:
+#     def uniqueXorTriplets(self, nums: List[int]) -> int:
+#         n = len(nums)
+#         if n <= 2:
+#             return n
+#         return 1 << n.bit_length()
+
+
+
+import math
+
 class Solution:
     def uniqueXorTriplets(self, nums: List[int]) -> int:
         n = len(nums)
         if n <= 2:
             return n
-        return 1 << n.bit_length()
+        
+        # Calculate the number of bits needed to represent n
+        highest_bit_index = int(math.log2(n)) + 1
+        
+        # 2^highest_bit_index
+        return 1 << highest_bit_index
