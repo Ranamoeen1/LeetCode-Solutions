e33cf2ae973cecffc9fee99b6be292238591b422
Rana Moeen <ranamoeenodeen@gmail.com>
2026-09-08 21:52:33 +0500

    Time: 0 ms (100%), Space: 19.4 MB (14.99%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.py
+++ 3870-count-commas-in-range/3870-count-commas-in-range.py
@@ -1,5 +1,19 @@
 class Solution:
     def countCommas(self, n: int) -> int:
-        if n < 1000:
-            return 0
-        return n - 999
+        ans = 0
+        threshold = 1000
+        
+        while n >= threshold:
+            ans += n - threshold + 1
+            threshold *= 1000
+            
+        return ans
+
+
+
+
+# class Solution:
+#     def countCommas(self, n: int) -> int:
+#         if n < 1000:
+#             return 0
+#         return n - 999
